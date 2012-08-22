0203101df97c82995d8d57d6066d0fade45f4ca4
The Machine <marcel@ubuntuminimal.(none)>
2012-08-22 00:10:07 -0500

    First commit for the mpi_hello example.

---
+++ mpi_hello.c
@@ -0,0 +1,15 @@
+/* C Example */
+#include <mpi.h>
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+	int rank, size;
+
+	MPI_Init (&argc, &argv); /* starts MPI */
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+	MPI_Comm_size (MPI_COMM_WORLD, &size);
+	printf("Hello world from process %d of %d\n", rank, size);
+	MPI_Finalize();
+	return 0;
+}
