da7d7ed5a90787bfc83dce78e39868e44bc9d1b0
Marcel Valdez <marcel.valdez@live.com>
2012-08-31 22:04:23 -0500

    Fixed the message printed

--- mpi_hello/mpi_hello.c
+++ mpi_hello/mpi_hello.c
@@ -7,10 +7,10 @@ int main (int argc, char* argv[])
 	int rank, size;
 
 	MPI_Init (&argc, &argv); /* starts MPI */
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 	MPI_Comm_size (MPI_COMM_WORLD, &size);
 	printf("Hello world from process %d of %d\n", rank, size);
 	printf("Rank: %d Size: %d\n", rank, size);
-	MPI_Comm_size (MPI_COMM_WORLD, &size);
 	MPI_Finalize();
 	return 0;
 }
