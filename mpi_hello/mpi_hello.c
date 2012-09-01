a0e0d0116c4006c6cc559e55bd0dbf71077f6121
Marcel Valdez <marcel.valdez@live.com>
2012-08-31 21:32:41 -0500

    Added print rank and size information

--- mpi_hello/mpi_hello.c
+++ mpi_hello/mpi_hello.c
@@ -7,6 +7,7 @@ int main (int argc, char* argv[])
 	int rank, size;
 
 	MPI_Init (&argc, &argv); /* starts MPI */
+	printf("Rank: %d Size: %d", rank, size);
 	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 	MPI_Comm_size (MPI_COMM_WORLD, &size);
 	printf("Hello world from process %d of %d\n", rank, size);
