fe25c02fe656fa19eb3784bf09aa2dde2e795eca
Marcel Valdez <marcel.valdez@live.com>
2012-09-04 17:22:02 -0500

    Added filename to be received

--- mpi_mandelbrot/src/mandelbrot.c
+++ mpi_mandelbrot/src/mandelbrot.c
@@ -30,10 +30,10 @@ int main(int arg_count, char * args[])
 	if (my_proc_index == 0)
 	{
 		if (rows > 100 || cols > 100) {
-			printf("Writing image file");
-			write_to_file(data, rows, cols, "mandelbrot.bmp");
+			printf("Writing image file\n");
+			write_to_file(data, rows, cols, args[0]);
 		} else {
-			printf("Printing mandelbrot");
+			printf("Printing mandelbrot\n");
 			print_mandelbrot(data, rows, cols);
 		}
 	}
